1e95596ca9c4c1059627f98ea650fb35eeef32d6
t0nn1x <antoonhrobust@gmail.com>
2023-07-29 20:54:16 +0200

    Updating data in the database

--- src/main/java/com/jdbc/JdbcTest.java
+++ src/main/java/com/jdbc/JdbcTest.java
@@ -2,11 +2,13 @@ package com.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JdbcTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Connection myConn = null;
         Statement myStmt = null;
         ResultSet myRs = null;
@@ -23,24 +25,74 @@ public class JdbcTest {
             // 2. Create a statement
             myStmt = myConn.createStatement();
 
-            // 3. Inserting data 
-            System.out.println("Inseting a new employee to database\n");
+            // 3. Check employee information
+            System.out.println("BEFORE THE UPDATE...");
+            displayEmployee(myConn, "John", "Doe");
+
+            // 4. Executing the update
+            System.out.println("Updating an employee\n");
             int rowsAffected = myStmt.executeUpdate(
-                "INSERT INTO employees " +
-                "(last_name, first_name, email, department, salary)" +
-                "VALUES " +
-                "('Anton', 'Khrobust', 'antonhrobust@gmail.com', 'DEV', '100000.00')"
+                "UPDATE employees " + 
+                "set email='johndoe1@gmail.com' " +
+                "WHERE last_name = 'Doe' and first_name = 'John'" 
                 );
 
-            // 4. Verify this by getting list of employees
-            myRs = myStmt.executeQuery("select * from employees");
+            // 5. Check employee information
+            System.out.println("AFTER THE UPDATE...");
+            displayEmployee(myConn, "John", "Doe");
 
-            // 5. Process the result set
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            close(myConn, myStmt, myRs);
+        }
+    }
+
+    private static void displayEmployee(Connection myConn, String firstName, String lastName) throws SQLException {
+        PreparedStatement myStmt = null;
+        ResultSet myRs = null;
+
+        try {
+            // 1. Prepare statement
+            myStmt = myConn.prepareStatement("select last_name, first_name, email from employees where last_name=? and first_name=?");
+
+            // 2. Set the parameters
+            myStmt.setString(1, lastName);
+            myStmt.setString(2, firstName);
+
+            // 3. Execute SQL query
+            myRs = myStmt.executeQuery();
+
+            // 4. Process the result set
             while (myRs.next()) {
-                System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
+                String theLastName = myRs.getString("last_name");
+                String theFirstName = myRs.getString("first_name");
+                String email = myRs.getString("email");
+
+                System.out.printf("%s, %s, %s\n", theLastName, theFirstName, email);
             }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            close(myStmt, myRs);
         }
+    }
+
+    private static void close(Connection myConn, Statement myStmt, ResultSet myRs) throws SQLException {
+		if (myRs != null) {
+			myRs.close();
+		}
+
+		if (myStmt != null) {
+			myStmt.close();
+		}
+
+		if (myConn != null) {
+			myConn.close();
+		}
+	}
+
+    private static void close(Statement myStmt, ResultSet myRs) throws SQLException {
+        close(null, myStmt, myRs);		
     }
 }
