a61dbf7e0f360d1785cd75b2f2f6cdb72d2f8f07
t0nn1x <antoonhrobust@gmail.com>
2023-07-29 23:31:22 +0200

    Creating Prepare Statements

--- src/main/java/com/jdbc/JdbcTest.java
+++ src/main/java/com/jdbc/JdbcTest.java
@@ -10,7 +10,7 @@ import java.sql.Statement;
 public class JdbcTest {
     public static void main(String[] args) throws SQLException {
         Connection myConn = null;
-        Statement myStmt = null;
+        PreparedStatement myStmt = null;
         ResultSet myRs = null;
 
         String dbUrl = "jdbc:mysql://localhost:3306/demo";
@@ -23,22 +23,33 @@ public class JdbcTest {
             System.out.println("Database connection successful!\n");
 
             // 2. Create a statement
-            myStmt = myConn.createStatement();
-
-            // 3. Check employee information
-            System.out.println("BEFORE THE DELETE...");
-            displayEmployee(myConn, "John", "Doe");
-
-            // 4. Executing the update
-            System.out.println("DELETING an employee\n");
-            int rowsAffected = myStmt.executeUpdate(
-                "DELETE FROM employees " + 
-                "WHERE last_name = 'Doe' and first_name = 'John'" 
+            myStmt = myConn.prepareStatement(
+                "SELECT * FROM employees WHERE salary > ? AND department = ?"
                 );
 
-            // 5. Check employee information
-            System.out.println("AFTER THE DELETE...");
-            displayEmployee(myConn, "John", "Doe");
+            // 3.1. Set the parameters
+            myStmt.setDouble(1, 80000);
+            myStmt.setString(2, "Legal");
+
+            // 4. Execute SQL query
+            myRs = myStmt.executeQuery();
+
+            // 5.1 Display the result
+            display(myRs);
+
+            // Reusing the prepared statement with another values
+            System.out.println("Reusing prepared statement");
+
+            // 3.2. Set the new parameters
+            myStmt.setDouble(1, 25000);
+            myStmt.setString(2, "HR");
+
+            // 4.2. Execute SQL query
+            myRs = myStmt.executeQuery();
+
+            // 5.2. Display the result
+            display(myRs); 
+
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -47,33 +58,14 @@ public class JdbcTest {
         }
     }
 
-    private static void displayEmployee(Connection myConn, String firstName, String lastName) throws SQLException {
-        PreparedStatement myStmt = null;
-        ResultSet myRs = null;
+    private static void display(ResultSet myRs) throws SQLException {
+        while (myRs.next()) {
+            String lastName = myRs.getString("last_name");
+            String firstName = myRs.getString("first_name");
+            double salary = myRs.getDouble("salary");
+            String department = myRs.getString("department");
 
-        try {
-            // 1. Prepare statement
-            myStmt = myConn.prepareStatement("select last_name, first_name, email from employees where last_name=? and first_name=?");
-
-            // 2. Set the parameters
-            myStmt.setString(1, lastName);
-            myStmt.setString(2, firstName);
-
-            // 3. Execute SQL query
-            myRs = myStmt.executeQuery();
-
-            // 4. Process the result set
-            while (myRs.next()) {
-                String theLastName = myRs.getString("last_name");
-                String theFirstName = myRs.getString("first_name");
-                String email = myRs.getString("email");
-
-                System.out.printf("%s, %s, %s\n", theLastName, theFirstName, email);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            close(myStmt, myRs);
+            System.out.printf("%s, %s, %.2f, %s\n", lastName, firstName, salary, department);
         }
     }
 
