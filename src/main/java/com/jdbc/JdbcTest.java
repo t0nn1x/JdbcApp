1c4db0c4b1aac7debc288cf3083ba7b4aedbdc73
t0nn1x <antoonhrobust@gmail.com>
2023-07-29 17:29:33 +0200

    Inserting Data into DB

--- src/main/java/com/jdbc/JdbcTest.java
+++ src/main/java/com/jdbc/JdbcTest.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import com.mysql.cj.jdbc.Driver;
 
 public class JdbcTest {
     public static void main(String[] args) {
@@ -17,14 +16,26 @@ public class JdbcTest {
         String pass = "password";
 
         try {
+            // 1. Get a connection to database
             myConn = DriverManager.getConnection(dbUrl, user, pass);
-
             System.out.println("Database connection successful!\n");
 
+            // 2. Create a statement
             myStmt = myConn.createStatement();
 
+            // 3. Inserting data 
+            System.out.println("Inseting a new employee to database\n");
+            int rowsAffected = myStmt.executeUpdate(
+                "INSERT INTO employees " +
+                "(last_name, first_name, email, department, salary)" +
+                "VALUES " +
+                "('Anton', 'Khrobust', 'antonhrobust@gmail.com', 'DEV', '100000.00')"
+                );
+
+            // 4. Verify this by getting list of employees
             myRs = myStmt.executeQuery("select * from employees");
 
+            // 5. Process the result set
             while (myRs.next()) {
                 System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
             }
