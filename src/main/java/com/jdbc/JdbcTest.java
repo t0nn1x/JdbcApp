e7168a5b561a290b4725be0d5b8509e9d6066d89
t0nn1x <antoonhrobust@gmail.com>
2023-08-01 15:12:16 +0200

    Storing Procedures(INOUT Parameters)

--- src/main/java/com/jdbc/JdbcTest.java
+++ src/main/java/com/jdbc/JdbcTest.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.sql.CallableStatement;
 
 public class JdbcTest {
@@ -23,71 +24,27 @@ public class JdbcTest {
             System.out.println("Database connection successful!\n");
 
             String theDepartment = "Engineering";
-            int theIncreaseAmount = 10000;
 
-            // 2. Show salaries before
-            System.out.println("Salaries before: ");
-            showSalaries(myConn, theDepartment);
+            // 2. Prepare the stored procedure call
+            myStmt = myConn.prepareCall("{call greet_the_department(?)}");
 
-            // 3. Prepare the stored procedure call
-            myStmt = (CallableStatement) myConn.prepareCall(
-                    "{call increase_salaries_for_department(?, ?)}");
-
-            // 4. Set the params
+            // 3. Set the parameters
+            myStmt.registerOutParameter(1, Types.VARCHAR);
             myStmt.setString(1, theDepartment);
-            myStmt.setDouble(2, theIncreaseAmount);
 
-            // 5. Call stored procedure
-            System.out.println("\nCalling stored procedure");
+            // 4. Call stored procedure
+            System.out.println("Calling stored procdeure. greet_the_department('" + theDepartment + "')");
             myStmt.execute();
-            System.out.println("Finished calling stored procedure");
+            System.out.println("Finishing calling stored procedure");
 
-            // 6. Show salaries after
-            System.out.println("\nSalaries after: ");
-            showSalaries(myConn, theDepartment);
+            // 6. Get the value of INOUT parameter
+            String theResult = myStmt.getString(1);
+            System.out.println("\nThe result = " + theResult);
 
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             close(myConn, myStmt, myRs);
-        }
-    }
-
-    private static void showSalaries(Connection myConn, String theDepartment) throws SQLException {
-        CallableStatement myStmt = null;
-        ResultSet myRs = null;
-
-        try {
-            // Prepare statement
-            myStmt = (CallableStatement) myConn.prepareCall(
-                    "{call get_employees_for_department(?)}");
-
-            // Set the parameter
-            myStmt.setString(1, theDepartment);
-
-            // Call stored procedure
-            myStmt.execute();
-
-            // Get the result set
-            myRs = myStmt.getResultSet();
-
-            // Display the result set
-            display(myRs);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            close(myStmt, myRs);
-        }
-    }
-
-    private static void display(ResultSet myRs) throws SQLException {
-        while (myRs.next()) {
-            String lastName = myRs.getString("last_name");
-            String firstName = myRs.getString("first_name");
-            double salary = myRs.getDouble("salary");
-            String department = myRs.getString("department");
-
-            System.out.printf("%s, %s, %.2f, %s\n", lastName, firstName, salary, department);
         }
     }
 
